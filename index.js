0d9843c6e9aa0d1c915d3a87cca52508054bee51
Scott (DC)  Christensen <schristensen10@gmail.com>
2019-09-04 09:26:52 -0600

    As far as I could get

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-alert ("Connected")
+
 var rock = document.getElementById("rock");
 var paper = document.getElementById("paper")
 var scissors = document.getElementById("scissors")
 var userChoice = ""
+var result = "";
 var compChoice = Math.random();
-  if (compChoice < 0.34) {
-    compChoice = "rock"
-  }
-  else if (compChoice <= 0.64) {
-    compChoice = "paper"
-  }
-  else {
-    compChoice = "scissors"
-  }
 
-var compare = function(choice1, choice2) {
+if (compChoice < 0.34) {
+  compChoice = "rock"
+}
+else if (compChoice <= 0.64) {
+  compChoice = "paper"
+}
+else {
+  compChoice = "scissors"
+}
+
+  var compare = function(choice1, choice2) {
   if(choice1 === choice2) {
     return "It's a tie!"
   }
-}
+
   if(choice1 === "rock") {
     if(choice2 === "scissors") {
      return "rock wins";
-  }   else {
+    }   else {
         return "paper wins";
+    }
   }
-}
-if (choice1 === "paper") {
+
+  if (choice1 === "paper") {
   if (choice2 === "rock") {
       return "paper wins";
   } else {
@@ -46,14 +49,20 @@ if (choice1 === "paper") {
 }
   rock.addEventListener("click", function(){
   userChoice = this.id;
+  compare(userChoice, compChoice);
 });
 
 paper.addEventListener("click", function(){
   userChoice = this.id; 
+  compare(userChoice, compChoice);
+
 });
 
 scissors.addEventListener("click", function(){
   userChoice = this.id;
+  result = compare(userChoice, compChoice);
+  alert(result);
+
 });
 
 // console.log("userChoice: " + userChoice);
