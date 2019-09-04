9bf9c443fdd701c51984e9a35040295fcbd783bb
Scott (DC)  Christensen <schristensen10@gmail.com>
2019-09-04 15:45:11 -0600

    Started Over

--- index.js
+++ index.js
@@ -1,72 +1,39 @@
-
 var rock = document.getElementById("rock");
-var paper = document.getElementById("paper")
+var paper = document.getElementById("paper");
 var scissors = document.getElementById("scissors")
-var userChoice = ""
-var result = "";
-var compChoice = Math.random();
+var userChoice = [];
+var rpsChoice = [ "rock", "paper", "scissors"];
 
-if (compChoice < 0.34) {
-  compChoice = "rock"
-}
-else if (compChoice <= 0.64) {
-  compChoice = "paper"
-}
-else {
-  compChoice = "scissors"
-}
 
-  var compare = function(choice1, choice2) {
-  if(choice1 === choice2) {
-    return "It's a tie!"
-  }
-
-  if(choice1 === "rock") {
-    if(choice2 === "scissors") {
-     return "rock wins";
-    }   else {
-        return "paper wins";
-    }
-  }
-
-  if (choice1 === "paper") {
-  if (choice2 === "rock") {
-      return "paper wins";
-  } else {
-      if (choice2 === "scissors") {
-          return "scissors wins";
-      }
-  }
-  if (choice1 === "scissors") {
-      if (choice2 === "rock") {
-          return "rock wins";
-      } else {
-          if (choice2 === "paper") {
-              return "scissors wins";
-          }
-      }
-  }
-}
-  rock.addEventListener("click", function(){
+rock.addEventListener( "click", function() {
   userChoice = this.id;
-  compare(userChoice, compChoice);
+  console.log(`you clicked ${userChoice}.`)
+  compChoice()
 });
 
-paper.addEventListener("click", function(){
-  userChoice = this.id; 
-  compare(userChoice, compChoice);
-
-});
-
-scissors.addEventListener("click", function(){
+paper.addEventListener( "click", function() {
   userChoice = this.id;
-  result = compare(userChoice, compChoice);
-  alert(result);
-
+  console.log(`you clicked ${userChoice}.`)
+  
 });
 
-// console.log("userChoice: " + userChoice);
-// console.log("compchoice: " + compChoice);
-// get comp choice
-// display those choices to the browser
-// decide who wins
+scissors.addEventListener( "click", function() {
+  userChoice = this.id;
+  console.log(`you clicked ${userChoice}.`)
+  compChoice()
+});
+
+function compChoice() {
+  compResult = rpsChoice[Math.floor(Math.random()*rpsChoice.length)]
+  console.log(`computer chose ${compResult}.`)
+  result()
+}
+
+function result(){
+  if (userChoice === compChoice)
+}
+
+// console.log("Your choice is ${userChoice}, the computers choice is ${compChoice}." );
+  // get comp choice
+  // display those choices to the browser
+  // decide who wins
