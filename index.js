806638f05e5228ed7194fb2a89fa064d5c00887b
Scott (DC)  Christensen <schristensen10@gmail.com>
2019-09-04 19:30:03 -0600

    Best I could do, needs styling

--- index.js
+++ index.js
@@ -2,38 +2,58 @@ var rock = document.getElementById("rock");
 var paper = document.getElementById("paper");
 var scissors = document.getElementById("scissors")
 var userChoice = [];
-var rpsChoice = [ "rock", "paper", "scissors"];
+var rpsChoice = ["rock", "paper", "scissors"];
+var compResult
 
 
-rock.addEventListener( "click", function() {
+rock.addEventListener("click", function () {
   userChoice = this.id;
-  console.log(`you clicked ${userChoice}.`)
+  console.log(`you chose ${userChoice}.`)
   compChoice()
 });
 
-paper.addEventListener( "click", function() {
+paper.addEventListener("click", function () {
   userChoice = this.id;
-  console.log(`you clicked ${userChoice}.`)
-  
+  console.log(`you chose ${userChoice}.`)
+  compChoice()
 });
 
-scissors.addEventListener( "click", function() {
+scissors.addEventListener("click", function () {
   userChoice = this.id;
-  console.log(`you clicked ${userChoice}.`)
+  console.log(`you chose ${userChoice}.`)
   compChoice()
 });
 
 function compChoice() {
-  compResult = rpsChoice[Math.floor(Math.random()*rpsChoice.length)]
+  compResult = rpsChoice[Math.floor(Math.random() * rpsChoice.length)]
   console.log(`computer chose ${compResult}.`)
   result()
 }
 
-function result(){
-  if (userChoice === compChoice)
-}
+var finalResult = document.getElementById("result")
 
-// console.log("Your choice is ${userChoice}, the computers choice is ${compChoice}." );
-  // get comp choice
-  // display those choices to the browser
-  // decide who wins
+function result() {
+  if (userChoice === compResult) {
+    finalResult.innerHTML = `It's a tie! You chose ${userChoice}, Computer chose ${compResult}`;
+
+  } else if (userChoice === 'paper' && compResult === 'rock') {
+    finalResult.innerHTML = `You win! You chose ${userChoice}, Computer chose ${compResult}`;
+
+  } else if (userChoice === 'paper' && compResult === 'scissors') {
+    finalResult.innerHTML = `You lose! You chose ${userChoice}, Computer chose ${compResult}`;
+
+  } else if (userChoice === 'scissors' && compResult === 'paper') {
+    finalResult.innerHTML = `You win! You chose ${userChoice}, Computer chose ${compResult}`;
+
+  } else if (userChoice === 'scissors' && compResult === 'rock') {
+    finalResult.innerHTML = `You lose! You chose ${userChoice}, Computer chose ${compResult}`;
+
+  } else if (userChoice === 'rock' && compResult === 'scissors') {
+    finalResult.innerHTML = `You win! You chose ${userChoice}, Computer chose ${compResult}`;
+
+  } else if (userChoice === 'rock' && compResult === 'paper') {
+    finalResult.innerHTML = `You lose! You chose ${userChoice}, Computer chose ${compResult}`;
+  } else {
+  console.log("Error, please try again.");
+  }
+}
